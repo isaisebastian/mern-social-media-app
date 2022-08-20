53f9536b1652c9dd0fd163c948d920204dae815b
Sebastian-Cristian Isai <sebastianisai14@outlook.com>
2022-08-20 17:10:45 +0300

    semantic UI and display posts

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React from 'react';
-//import 'semantic-ui-css/semantic.min.css';
+
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import { Container } from 'semantic-ui-react';
+
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import MenuBar from './components/MenuBar';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route exact path='/register' element={<Register />} />
-        <Route exact path='/login' element={<Login />} />
-      </Routes>
+      <Container>
+        <MenuBar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/register' element={<Register />} />
+          <Route exact path='/login' element={<Login />} />
+        </Routes>
+      </Container>
     </Router>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client'
 import App from './App';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
@@ -12,9 +12,7 @@ const client = new ApolloClient({
 });
 
 root.render(
-  <StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  </StrictMode>
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
 );
