36c4135df89d73ac47c4e401b70b81c7dc33f43d
Sebastian-Cristian Isai <sebastianisai14@outlook.com>
2022-08-23 23:38:11 +0300

    add posts feature

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'
 import App from './App';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { HttpLink, ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
+const link = new HttpLink({
+  uri: "http://localhost:5000/"
+  // Additional options
+});
+
+const authLink = setContext(() => {
+  const token = localStorage.getItem('jwtToken');
+  return{ 
+    headers: {
+      Authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+});
+
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/', 
+  link: authLink.concat(link), 
   cache: new InMemoryCache(),
 });
+
 
 root.render(
   <ApolloProvider client={client}>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import { gql, useQuery } from '@apollo/client';
+import React, { useContext } from 'react'
+import { useQuery } from '@apollo/client';
 
 import { Grid } from 'semantic-ui-react';
 import PostCard from '../components/PostCard';
 
+import { AuthContext } from '../context/auth';
+import PostForm from '../components/PostForm';
+import { FETCH_POSTS_QUERY } from '../util/graphql';
+
 function Home() {
   const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+  const { user } = useContext(AuthContext);
 
   return (
     <Grid columns={3}>
@@ -13,6 +18,11 @@ function Home() {
         <h1>Recent Posts</h1>
       </Grid.Row>
       <Grid.Row>
+        {user && (
+          <Grid.Column>
+            <PostForm />
+          </Grid.Column>
+        )}
         {loading ? (
           <h1>Loading posts..</h1>
         ) : (
@@ -27,19 +37,6 @@ function Home() {
   )
 }
 
-const FETCH_POSTS_QUERY = gql`
-{
-  getPosts{
-    id body createdAt username likeCount
-    likes {
-      username 
-    }
-    commentCount
-    comments {
-      id username createdAt body
-    }
-  }
-}
-`;
+
 
 export default Home
